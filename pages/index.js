e726700c6fc331e16efeeb324f1c2150f3efc833
Baris Balli <barisbll.dev@gmail.com>
2022-04-09 19:02:28 +0200

    scss logic added with mixins

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
+import { lightTheme } from "../constants/theme";
 
 export default function Home() {
   return (
-    <>
+    <div className={`${styles.home}`} style={lightTheme}>
       <p style={{ fontSize: "5rem", fontWeight: 600 }}>Hello World!</p>
-    </>
+    </div>
   );
 }
